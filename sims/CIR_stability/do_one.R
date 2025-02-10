1eebebe0f8b552ebfd3e4601665277678708eb5a
Charlie Wolock <cwolock@gmail.com>
2025-02-09 22:41:24 -0500

    sim update

--- sims/CIR_stability/do_one.R
+++ sims/CIR_stability/do_one.R
@@ -13,7 +13,7 @@ do_one <- function(n, method){
   # scale = exp(0.4*w[,1] - 0.2*w[,2] + beta_int*w[,1]*w[,2]))
   y <- rweibull(n,
                 shape = 0.75,
-                scale = exp(0.4*w[,1] - 0.2*w[,2] + 0.1*w[,3] + beta_int/4*w[,1]*w[,2] + beta_int/4*w[,1]*w[,3] - beta_int/4*w[,2]*w[,3]))
+                scale = exp(0.4*w[,1] - 0.2*w[,2] + 0.1*w[,3] + beta_int/5*w[,1]*w[,2] + beta_int/5*w[,1]*w[,3] - beta_int/5*w[,2]*w[,3]))
   # y <- rlnorm(n, meanlog = 0.4*w[,1] - 0.2*w[,2] + beta_int*w[,1]*w[,2], sdlog = 1)
 
   # round c to nearest quantile of c, just so there aren't so many unique values
