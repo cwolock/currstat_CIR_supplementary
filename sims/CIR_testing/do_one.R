0fe2ed682ccf620007201b2848dd2e565a6951d6
Charlie Wolock <cwolock@gmail.com>
2025-03-01 10:48:09 -0500

    sim update

--- sims/CIR_testing/do_one.R
+++ sims/CIR_testing/do_one.R
@@ -1,15 +1,30 @@
-do_one <- function(n, missing_bound, method, eval_upper_bound){
+do_one <- function(n, theta, method){
+  missing_bound <- 1.65
+  eval_upper_bound <- 1.5
   start <- Sys.time()
-  beta_int <- 0.4
   w <- cbind(2*rbinom(n, size = 1, prob = 0.5)-1,
              2*rbinom(n, size = 1, prob = 0.5)-1,
              2*rbinom(n, size = 1, prob = 0.5)-1)
-  t <- rweibull(n,
+
+  mycop <- claytonCopula(param = theta, dim = 2)
+  u <- rCopula(n, mycop)
+
+  # tau = theta/(theta + 2)
+
+  # inverse cdf transform to get exponential marginals
+  t <- qweibull(p = u[,1],
                 shape = 0.75,
-                scale = exp(0.4*w[,1] - 0.2*w[,2] + 0.1*w[,3] + beta_int*w[,1]*w[,2] + beta_int*w[,1]*w[,3] - beta_int*w[,2]*w[,3]))
-  y <- rweibull(n,
+                scale = exp(0.4*w[,1] - 0.2*w[2] + 0.1*w[,3]))
+  y <- qweibull(p = u[,2],
                 shape = 0.75,
-                scale = exp(0.4*w[,1] - 0.2*w[,2] + 0.1*w[,3] + beta_int/5*w[,1]*w[,2] + beta_int/5*w[,1]*w[,3] - beta_int/5*w[,2]*w[,3]))
+                scale = exp(0.4*w[,1] - 0.2*w[2] + 0.1*w[,3]))
+
+  # t <- rweibull(n,
+  # shape = 0.75,
+  # scale = exp(0.4*w[,1] - 0.2*w[,2] + 0.1*w[,3] + beta_int*w[,1]*w[,2] + beta_int*w[,1]*w[,3] - beta_int*w[,2]*w[,3]))
+  # y <- rweibull(n,
+  # shape = 0.75,
+  # scale = exp(0.4*w[,1] - 0.2*w[,2] + 0.1*w[,3] + beta_int/5*w[,1]*w[,2] + beta_int/5*w[,1]*w[,3] - beta_int/5*w[,2]*w[,3]))
 
   # round c to nearest quantile of c, just so there aren't so many unique values
   quants <- quantile(y, probs = seq(0, 1, by = 0.02), type = 1)
@@ -26,93 +41,39 @@ do_one <- function(n, missing_bound, method, eval_upper_bound){
   }
   eval_region <- c(0, eval_upper_bound+0.125)
 
-
-  if (method == "npmle"){
-    dat_cc <- dat %>% filter(!is.na(delta))
-    y_vals <- sort(unique(dat_cc$y))
-    F_n <- stats::ecdf(dat_cc$y)
-    F_n_inverse <- function(t){
-      stats::quantile(dat_cc$y, probs = t, type = 1)
-    }
-    eval_cdf_upper <- mean(dat_cc$y <= eval_region[2])
-    eval_cdf_lower <- mean(dat_cc$y <= eval_region[1])
-    x <- sapply(seq(eval_cdf_lower, eval_cdf_upper, length.out = 101), F_n_inverse)
-    fit <- isoreg(dat_cc$y, dat_cc$delta)
-    xvals <- sort(fit$x)
-    yvals <- fit$yf
-    fn <- stepfun(xvals, c(yvals[1], yvals))
-    res <- data.frame(t = x)
-    res$cdf_estimate = apply(matrix(x), MARGIN = 1, FUN = fn)
-    res$cil <- NA
-    res$ciu <- NA
-  } else if (method == "npmle_survival"){
-    dat_cc <- dat %>% filter(!is.na(delta))
-
-    icen_form <- icenReg::cs2ic(time = dat_cc$y,
-                                eventOccurred = as.logical(dat_cc$delta))
-    icen_form[icen_form == 0] <- -Inf
-    surv_obj <- survival::Surv(time = icen_form[,1], time2 = icen_form[,2], type = "interval2")
-    fit_surv <- survfit(surv_obj ~ 1)
-
-    F_n <- stats::ecdf(dat_cc$y)
-    F_n_inverse <- function(t){
-      stats::quantile(dat_cc$y, probs = t, type = 1)
-    }
-    eval_cdf_upper <- mean(dat_cc$y <= eval_region[2])
-    eval_cdf_lower <- mean(dat_cc$y <= eval_region[1])
-    x <- sapply(seq(eval_cdf_lower, eval_cdf_upper, length.out = 101), F_n_inverse)
-    res <- data.frame(t = x)
-
-    res$cdf_estimate <- NA
-    res$cil <- NA
-    res$ciu <- NA
-    for (i in 1:nrow(res)){
-      closest <- which.min(abs(fit_surv$time - res$t[i]))
-      res$cdf_estimate[i] <- 1 - fit_surv$surv[closest]
-      res$cil[i] <- 1 - fit_surv$upper[closest]
-      res$ciu[i] <- 1 - fit_surv$lower[closest]
-    }
-  } else if (method == "cc"){
-    dat_cc <- dat %>% filter(!is.na(delta))
-    res <- survML::currstatCIR(time = dat_cc$y,
-                               event = dat_cc$delta,
-                               X = dat_cc[,3:5],
-                               SL_control = list(SL.library = c("SL.mean", "SL.glm", "SL.earth", "SL.gam", "SL.ranger"),
-                                                 V = 5,
-                                                 method = "method.NNLS"),
-                               HAL_control = list(n_bins = c(5,10),
-                                                  grid_type = c("equal_mass", "equal_range"),
-                                                  V = 5),
-                               eval_region = eval_region)
-    res$S_hat_est <- 1 - res$S_hat_est
-    temp_cil <- res$S_hat_cil
-    temp_ciu <- res$S_hat_ciu
-    res$S_hat_cil <- 1 - temp_ciu
-    res$S_hat_ciu <- 1 - temp_cil
-  } else if (method == "extended"){
+  if (method == "copula"){
+    res <- survML::currstatCIR_copula(time = dat$y,
+                                      event = dat$delta,
+                                      X = dat[,3:5],
+                                      SL_control = list(SL.library = c("SL.mean", "SL.glm", "SL.earth", "SL.gam", "SL.ranger"),
+                                                        V = 5,
+                                                        method = "method.NNLS"),
+                                      HAL_control = list(n_bins = c(5,10),
+                                                         grid_type = c("equal_mass", "equal_range"),
+                                                         V = 5),
+                                      eval_region = eval_region,
+                                      theta = theta)
+  } else if (method == "nocopula"){
     res <- survML::currstatCIR(time = dat$y,
-                               event = dat$delta,
-                               X = dat[,3:5],
-                               SL_control = list(SL.library = c("SL.mean", "SL.glm", "SL.earth", "SL.gam", "SL.ranger"),
-                                                 V = 5,
-                                                 method = "method.NNLS"),
-                               HAL_control = list(n_bins = c(5,10),
-                                                  grid_type = c("equal_mass", "equal_range"),
-                                                  V = 5),
-                               eval_region = eval_region)
-    res$S_hat_est <- 1 - res$S_hat_est
-    temp_cil <- res$S_hat_cil
-    temp_ciu <- res$S_hat_ciu
-    res$S_hat_cil <- 1 - temp_ciu
-    res$S_hat_ciu <- 1 - temp_cil
+                                      event = dat$delta,
+                                      X = dat[,3:5],
+                                      SL_control = list(SL.library = c("SL.mean", "SL.glm", "SL.earth", "SL.gam", "SL.ranger"),
+                                                        V = 5,
+                                                        method = "method.NNLS"),
+                                      HAL_control = list(n_bins = c(5,10),
+                                                         grid_type = c("equal_mass", "equal_range"),
+                                                         V = 5),
+                                      eval_region = eval_region)
   }
 
+
+  res$S_hat_est <- 1 - res$S_hat_est
+
   names(res) <- c("y", "cdf_estimate", "cil", "ciu")
-  res$n = n
-  res$missing_bound <- missing_bound
-  res$method <- method
-  res$eval_upper_bound <- eval_upper_bound
+  res$n <- n
   res$n_actual <- sum(!is.na(dat$delta))
+  res$theta <- theta
+  res$method <- method
 
   n <- 5e5
   w <- cbind(2*rbinom(n, size = 1, prob = 0.5) - 1,
@@ -120,7 +81,7 @@ do_one <- function(n, missing_bound, method, eval_upper_bound){
              2*rbinom(n, size = 1, prob = 0.5)-1)
   t <- rweibull(n,
                 shape = 0.75,
-                scale = exp(0.4*w[,1] - 0.2*w[,2] + 0.1*w[,3] + beta_int*w[,1]*w[,2] + beta_int*w[,1]*w[,3] - beta_int*w[,2]*w[,3]))
+                scale = exp(0.4*w[,1] - 0.2*w[,2] + 0.1*w[,3]))
   pop_truths <- seq(0, 1, length.out = 501)
   pop_taus <- quantile(t, probs = pop_truths)
 
