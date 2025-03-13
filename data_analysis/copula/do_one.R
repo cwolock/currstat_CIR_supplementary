2c63d23152a33e1492ef8629b77667c883f52edc
Charlie Wolock <cwolock@gmail.com>
2025-03-13 00:25:08 -0400

    update copula data analysis

--- data_analysis/copula/do_one.R
+++ data_analysis/copula/do_one.R
@@ -1,8 +1,21 @@
-do_one <- function(tau){
-  tau_to_theta <- function(tau){
-    return(-2*tau/(tau - 1))
+do_one <- function(tau, coplua = "frank"){
+  if (copula == "clayton"){
+    tau_to_theta <- function(tau){
+      return(-2*tau/(tau - 1))
+    }
+    theta <- tau_to_theta(tau)
+  } else if (copula == "frank"){
+    tau_to_theta <- function(tau){
+      thetas <- c(seq(-10, -0.01, by = 0.01), seq(0.01, 10, by = 0.01))
+      taus <- rep(NA, length(thetas))
+      for (i in 1:length(thetas)){
+        mycop <- copula::frankCopula(param = thetas[i])
+        taus[i] <- copula::tau(mycop)
+      }
+      return(thetas[which.min(abs(taus - tau))])
+    }
+    theta <- tau_to_theta(tau)
   }
-  theta <- tau_to_theta(tau)
 
   # dat <- readRDS("/Users/cwolock/Dropbox/UW/RESEARCH/paper_supplements/currstat_CIR_supplementary/data_analysis/copula/long_covid_truncated_120_021825_noinconclusives_fixedexpdates_keepinvitedbothyears.rds")
   # nuisances <- readRDS("/Users/cwolock/Dropbox/UW/RESEARCH/paper_supplements/currstat_CIR_supplementary/data_analysis/copula/saved_nuisances.rds")
@@ -25,6 +38,7 @@ do_one <- function(tau){
   # theta <- tau_to_theta(tau)
   # F_n_inverse(2^(1/theta))
 
+  # quant <- quantile(dat$y[dat$y < 120], probs = 2^(1/theta), na.rm = TRUE)
 
   res <- survML::currstatCIR_copula_saved_nuisances(time = dat$y,
                                                     event = dat$delta,
@@ -33,12 +47,13 @@ do_one <- function(tau){
                                                     f_sIx_n = nuisances$f_sIx_n,
                                                     f_s_n = nuisances$f_s_n,
                                                     F_sIx_n = nuisances$F_sIx_n,
-                                                    # g_n = nuisances$g_n,
                                                     Riemann_grid = nuisances$Riemann_grid,
                                                     n_eval_pts = 1001,
                                                     eval_region = c(0, 115),
-                                                    theta = theta)
+                                                    theta = theta,
+                                                    copula = copula)
   res$tau <- tau
+  res$copula <- copula
 
   return(res)
 
